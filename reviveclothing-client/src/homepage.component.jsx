281d3f6eeb1d58a5e402dd81ad85fd58f36a87b4
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-08 11:55:09 -0500

    created homepage components with menu options

---
+++ reviveclothing-client/src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>GO SHOPPING</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>JACKETS</h1>
+          <span className='subtitle'>GO SHOPPING</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SNEAKERS</h1>
+          <span className='subtitle'>GO SHOPPING</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>WOMENS</h1>
+          <span className='subtitle'>GO SHOPPING</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Mens</h1>
+          <span className='subtitle'>GO SHOPPING</span>
+        </div>
+      </div>
+    </div>
+  </div> 
+
+);
+
+export default Homepage;
