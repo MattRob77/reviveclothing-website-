b6b9408fef87df4763e138a8e70fe0272b0c890a
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-09 18:55:22 -0500

    created assets folder for png image file and imported in component

---
+++ reviveclothing-client/src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { } from '../../assets/r.png';
+
+import './header.styles.scss';
+
+const Header = () => (
+  <div className='header'>
+    <Link to="/">
+    </link>
+  </div>
+)
