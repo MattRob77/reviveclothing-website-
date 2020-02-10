a129255f4625848f8eba94144e95e655fc693089
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-10 12:30:49 -0500

    Added sign in button with styling without hover

--- reviveclothing-client/src/components/sign-in/sign-in.component.jsx
+++ reviveclothing-client/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -53,7 +54,7 @@ class SignIn extends React.Component {
             />
 
 
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'> Sign In</CustomButton>
         </form>
       </div>
     );
