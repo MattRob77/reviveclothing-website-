95d64b33d4bad6c41bde6649ed35653c7e161a49
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-10 08:35:43 -0500

    imported from signin page and created route for sign in

--- reviveclothing-client/src/App.js
+++ reviveclothing-client/src/App.js
@@ -3,8 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from'./pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
 
 
 
@@ -15,6 +17,7 @@ function App() {
       <switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
       </switch>
     </div>
 
