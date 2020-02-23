476d2784281333c8a1c1327e8b5829d09fa92293
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-23 14:12:25 -0500

    created notes for props explaing the spread

--- reviveclothing-client/src/components/custom-button/custom-button.component.jsx
+++ reviveclothing-client/src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,13 @@ import React from 'react'; //imported library
 
 import './custom-button.styles.scss'; //imported styling
 
-const CustomButton = ({ children, ...otherProps}) => ( //stateless functional component with children and otherprops property spreading otherprops in
-  <button className='custom-button' {...otherProps}> //if type submit button now will get the custom button
+const CustomButton = ({ children, ...otherProps}) => (
+  <button className='custom-button' {...otherProps}> 
     {children}
   </button>
 );
 
 export default CustomButton;
+
+//stateless functional component with children and otherprops property spreading otherprops in
+//if type submit button now will get the custom button
