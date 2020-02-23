cb4b4314967d7d1def64f62a1515528c790e6f3a
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-23 12:36:34 -0500

    Created notes to implement for form-input component.

--- reviveclothing-client/src/components/form-input/form-input.component.jsx
+++ reviveclothing-client/src/components/form-input/form-input.component.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'; //imported library
 
-import './form-input.styles.scss';
+import './form-input.styles.scss'; //imported styling
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="group">
+    <div className="group">
     <input className='form-input' onChange={handleChange} {...otherProps} />
     {
       label ?
@@ -16,3 +16,8 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
 )
 
 export default FormInput;
+
+//funcational component no state. Handle change, label and otherProps for destructuring
+//className group for grouping all the objects together
+//if value then apply shrink property otherwise empty string
+//shrink property is for what the user types in
--- reviveclothing-client/src/components/sign-in/sign-in.component.jsx
+++ reviveclothing-client/src/components/sign-in/sign-in.component.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from 'react'; //imported library
 import { connect } from 'react-redux';
 import { signIn } from "../../actions/currentUser";
-import FormInput from '../form-input/form-input.component';
+import FormInput from '../form-input/form-input.component'; //imported FormInput component
 import CustomButton from '../custom-button/custom-button.component';
 
-import './sign-in.styles.scss';
+import './sign-in.styles.scss'; //imported styling
 
-class SignIn extends React.Component {
+class SignIn extends React.Component { //class component to handle state
   constructor(props) {
     super(props);
 
     this.state = {
       email: '',
       password: ''
-
     }
   }
 
-  handleSubmit = event => {
-     event.preventDefault();
+  handleSubmit = event => { //calls the handle submit function
+     event.preventDefault(); //prevent the default action from firing
      this.props.signIn(this.state)
      this.setState({ email: '', password: ''})
-     this.props.history.push('/')
+     this.props.history.push('/')//once submitted gets redirected to homepage/history came from react-router
    }
 
-   handleChange = event => {
-     const { name, value } = event.target;
+   handleChange = event => {//calls the handle change for what the user types in
+     const { name, value } = event.target; //pulling the name and value off of event target
 
-     this.setState({ [name]: value});
+     this.setState({ [name]: value}); //setting the state if name is email/password will point to value typed in
    }
 
 
