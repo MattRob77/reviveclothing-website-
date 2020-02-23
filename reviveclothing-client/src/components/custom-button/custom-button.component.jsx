64a0f6a0f3d3ae9cf231c5248c4422481084aa2f
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-23 14:10:18 -0500

    Fixed notes

--- reviveclothing-client/src/components/custom-button/custom-button.component.jsx
+++ reviveclothing-client/src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'; //imported library
 
-import './custom-button.styles.scss';
+import './custom-button.styles.scss'; //imported styling
 
-const CustomButton = ({ children, ...otherProps}) => (
-  <button className='custom-button' {...otherProps}>
+const CustomButton = ({ children, ...otherProps}) => ( //stateless functional component with children and otherprops property spreading otherprops in
+  <button className='custom-button' {...otherProps}> //if type submit button now will get the custom button
     {children}
   </button>
 );
 
-export default CustomButton; 
+export default CustomButton;
