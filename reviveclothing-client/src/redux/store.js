c0c32a8cc9cb9860ac593418e7a8b0ea7c2d0593
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-23 20:47:03 -0500

    Created notes for store explaining creating the store & DevTools.

--- reviveclothing-client/src/redux/store.js
+++ reviveclothing-client/src/redux/store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from '../redux/root-reducer';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose} from 'redux'; //imported createStore & Middleware
+import rootReducer from '../redux/root-reducer'; //imported root-reducer connecting it to the store
+import thunk from 'redux-thunk'; //imported thunk middleware
 
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
-compose; //DevTools
+compose; //applied Redux Dev-Tools for debugging
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+//creates store passing functions of rootReducer, componseEnhancers and Middleware
 
 export default store;
