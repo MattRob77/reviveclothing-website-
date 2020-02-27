5c39a480edd39bc0a954633498c235fde8fa056f
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-27 09:36:35 -0500

    Changed cartReducer

--- reviveclothing-client/src/redux/cart/cartReducer.js
+++ reviveclothing-client/src/redux/cart/cartReducer.js
@@ -2,9 +2,6 @@ export default(state=[], action) => {
   switch(action.type) {
     case 'Add_To_Cart':
       return [...state, action.payload]
-    case 'Remove_From_Cart':
-      let newCart = state.filter(cart => cart.id !==action.payload)
-      return [...newCart]
     default:
       return state
   }
