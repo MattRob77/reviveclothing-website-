2b76a45165fa76335940639c59a36044b33c8a3f
MattRob77 <Matt_Robinson1@yahoo.com>
2020-03-05 13:41:59 -0500

    Added functionality to persists the cart data on refresh.

--- reviveclothing-client/src/index.js
+++ reviveclothing-client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom'; //Imported for restful routing
 import { BrowserRouter } from 'react-router-dom'; //imported component gives routing funtionality to application
 import { Provider } from 'react-redux'; //imported  Redux Provider allowing access to store to any components
-import store from './redux/store'; //imported store(state)
+import { PersistGate } from 'redux-persist/integration/react'; //This delays the rendering of your app’s UI until your persisted state has been retrieved and saved to redux.
+
+
+import { store, persistor } from './redux/store'; //imported store(state)
 
 import './index.css'; //imported styling
 import App from './App'; //imported app connecting it to index
@@ -14,7 +17,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
  <Provider store={store}>
    <BrowserRouter>
+    <PersistGate persistor={persistor}>
       <App />
+    </PersistGate>
     </BrowserRouter>
  </Provider>
 ,  document.getElementById('root') //grabs elements from HTML inserting App
--- reviveclothing-client/src/redux/root-reducer.js
+++ reviveclothing-client/src/redux/root-reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //local storage in window browser
 
 import cartReducer from './cart/cartReducer';
 import productsReducer from './products/productsReducer';
@@ -6,13 +8,20 @@ import currentUser from './user/currentUser';
 import signUp from "./signUpForm/signUp";
 
 
-export default combineReducers({
-  cart: cartReducer, 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'currentUser']
+}
+
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
   products: productsReducer,
   currentUser: currentUser,
   signUpForm: signUp,
+})
 
-
-});
+export default persistReducer(persistConfig, rootReducer);
 
 //Root-Reducer is base object that is all of our state in the application
