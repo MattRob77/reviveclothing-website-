5f910bcdc67dc80c2b9faf372329decc3dc86d1c
MattRob77 <Matt_Robinson1@yahoo.com>
2020-03-06 17:27:13 -0500

    Continued to implement routing for items.

--- reviveclothing-client/src/pages/collection/collection.component.jsx
+++ reviveclothing-client/src/pages/collection/collection.component.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shopSelectors';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className='collection-page'>
     <h2>COLLECTION PAGE</h2>
   </div>
 );
 
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- reviveclothing-client/src/redux/shop/shopSelectors.js
+++ reviveclothing-client/src/redux/shop/shopSelectors.js
@@ -1,8 +1,25 @@
 import { createSelector } from 'reselect';
 
+const Collection_ID_MAP = {
+  Mens: 1,
+  New: 2,
+  New: 3,
+  Womens: 4
+};
+
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
-  shop => shop.collections 
-)
+  shop => shop.collections
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections =>
+    collections.find(
+      collection => collection.id === Collection_ID_MAP[collectionUrlParam]
+    )
+  );
